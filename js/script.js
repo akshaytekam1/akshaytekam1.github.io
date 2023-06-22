955ee776b80306086a2bd6de23d708770fb0d6df
AKSHAY KUMAR TEKAM <46445922+akshaytekam1@users.noreply.github.com>
2023-06-22 20:29:54 +0530

    firsrt commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* ===================== typing animation =========================*/
+/* var typed = new Typed(".typing", {  */
+
+const typed = new Typed('.typing', {
+    strings: ["Web Development","App Development","Laravel","Firebase","Mongo-DB","Shell Scripting",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 600,
+    loop: true
+})
